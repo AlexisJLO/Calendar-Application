89259bdd5e6aa24d8c5f560d181e7723ebbba0c7
Alexis Lopez <lopez111alex@gmail.com>
2023-08-23 23:01:39 -0700

    Updated script.js
    added the current date function and a function to update the classes for the hours depending on the time

--- Develop/script.js
+++ Develop/script.js
@@ -19,15 +19,33 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  function updateBlockClasses() {
+    var currentHour = dayjs().hour();
+    $('.time-block').each(function () {
+      var blockHour = parseInt($(this).attr('id').split('-')[1]);
+      if (blockHour < currentHour) {
+        $(this).removeClass('present future').addClass('past');
+      } else if (blockHour === currentHour) {
+        $(this).removeClass('past future').addClass('present');
+      } else {
+        $(this).removeClass('past present').addClass('future');
+      }
+    });
+  }
+
+  updateBlockClasses();
   
-
-
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  for (var i = 9; i <= 17; i++) {
+    var savedEvent = localStorage.getItem('event-' + i);
+    $('#hour-' + i + ' .description').val(savedEvent);
+  }
 
+  // TODO: Add code to display the current date in the header of the page.
+  var currentDate = dayjs().format('dddd, MMMM D');
+  $('#currentDay').text(currentDate);
   
 });
 
